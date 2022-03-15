8c84e41ab5982a04d3f90e2dfe34c222c4c695c5
Jbasilio55 <j.l.basilio55@gmail.com>
2022-03-15 17:51:35 -0400

    added JS and fixed CSS

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+let editedPlayer = 0
+let activePlayer = 0
+
+const players = [
+    {
+        name: "",
+        symbol: "X"
+    },
+    {
+        name: "",
+        symbol: "O"
+    }
+]
+
+const playerConfigOverlay = document.getElementById("config-overlay")
+const backdrop = document.getElementById("backdrop")
+const formElement = document.querySelector("form")
+const errorsOutput = document.getElementById("config-errors")
+const gameArea = document.getElementById("active-game")
+const activePlayerName = document.getElementById("active-player-name")
+
+const editPlayer1Btn = document.getElementById("edit-player-1-btn")
+const editPlayer2Btn = document.getElementById("edit-player-2-btn")
+const cancelConfigBtn = document.getElementById("cancel-config-btn")
+const startNewGameBtn = document.getElementById("start-game-btn")
+// const gameFieldElements = document.querySelectorAll("#game-board li")
+const gameBoard = document.querySelectorAll("#game-board")
+
+
+editPlayer1Btn.addEventListener("click", openPlayerConfig)
+editPlayer2Btn.addEventListener("click", openPlayerConfig)
+
+cancelConfigBtn.addEventListener("click", closePlayerConfig)
+backdrop.addEventListener("click", closePlayerConfig)
+formElement.addEventListener("submit", savePlayerConfig)
+startNewGameBtn.addEventListener("click", startNewGame)
+
+// for(let gameFieldElement of gameFieldElements){
+//     gameFieldElement.addEventListener("click", selectGameField)
+// }
+
+gameBoard.addEventListener("click", selectGameField)
